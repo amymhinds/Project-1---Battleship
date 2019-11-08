93a669ca6e4faa5b6dc961bf7c8867bc6d5566f2
Amy Hinds <amyhinds@Amys-MacBook-Air.local>
2019-11-08 11:44:05 -0800

    fixes variable misspelling

--- js/main.js
+++ js/main.js
@@ -376,7 +376,7 @@ function compTurn() {
             /*if the ship array in the userShips object is completely hit, it is sunk */
             if(userShips[ShipName].every(x=> {
                 return x==='hit' })){
-                   checkCompWinnerArray.push(ShipName);
+                   compCheckWinnerArray.push(ShipName);
                 }
             /*change you won alert to message on board
             if the winner array contains all ships, comp won */ 
